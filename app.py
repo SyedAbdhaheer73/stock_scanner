ea6efc74a780861d8f55dbba151a3498260b9548
Syed Abdhaheer <mail4abdhaheer@gmail.com>
2026-01-10 04:27:51 +0530

    Adding personal touch

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def get_universe():
 
 UNIVERSE = get_universe()
 st.caption(f"Universe size: {len(UNIVERSE)} stocks")
+st.caption(f"Made with ❤️ by Syed Abdhaheer")
 
 # -------- DATA FRESHNESS --------
 _, market_ts = fetch_price("^NSEI")
